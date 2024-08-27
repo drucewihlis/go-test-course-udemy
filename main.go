3b911db43f9f3c62c14ca3a8088b6b35389c310d
anvi <avikhlyantsev@fix.ru>
2024-08-27 17:11:47 +0300

    3.done. 82.9% covered

--- main.go
+++ main.go
@@ -1,13 +1,80 @@
 // go run .
 package main
 
-import "fmt"
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "io"
+    "strconv"
+    "strings"
+)
 
 func main() {
-    n := 3
+    // print a welcome
+    intro()
 
-    _, msg := isPrime(n)
-    fmt.Println(msg)
+    // create a channel to indicate when the user wants to quit
+    doneChan := make(chan bool)
+
+    // start a goroutine to read user input and run program
+    go readUserInput(os.Stdin, doneChan)
+
+    // block until doneChan gets a value
+    <-doneChan
+
+    // close the channel
+    close(doneChan)
+
+    // say goodbye
+    fmt.Println("Goodbye.")
+}
+
+func readUserInput(in io.Reader, doneChan chan bool) {
+    scanner := bufio.NewScanner(in)
+
+    for {
+        res, done := checkNumbers(scanner)
+
+        if done {
+            doneChan <- true
+            return
+        }
+
+        fmt.Println(res)
+        prompt()
+    }
+}
+
+func checkNumbers(scanner *bufio.Scanner) (string, bool) {
+    // read user input
+    scanner.Scan()
+
+    // check to see if user wants to quit
+    if strings.EqualFold(scanner.Text(), "q") {
+        return "", true
+    }
+
+    // try to convert what user typed into int
+    numToCheck, err := strconv.Atoi(scanner.Text())
+    if err != nil {
+        return "Please enter a whole number!", false
+    }
+
+    _, msg := isPrime(numToCheck)
+
+    return msg, false
+}
+
+func intro() {
+    fmt.Println("Is it Prime?")
+    fmt.Println("------------")
+    fmt.Println("Enter a whole number, and we'll tell you if it is a prime number or not. Enter q to quit")
+    prompt()
+}
+
+func prompt() {
+    fmt.Print("-> ")
 }
 
 func isPrime(n int) (bool, string) {
--- main_test.go
+++ main_test.go
@@ -3,9 +3,19 @@
 // go test -coverprofile=coverage.out .
 // go tool cover -html=coverage.out  // should open browser, does not work for me
 // go tool cover -html=coverage.out -o coverage.html // generates html
+
+// go test -coverprofile=coverage.out . && go tool cover -html=coverage.out -o coverage.html
+
 package main
 
-import "testing"
+import (
+    "bufio"
+    "os"
+    "testing"
+    "io"
+    "strings"
+    "bytes"
+)
 
 func Test_isPrime(t *testing.T) {
     primeTests := []struct {
@@ -35,4 +45,99 @@ func Test_isPrime(t *testing.T) {
             t.Errorf("%s: expected `%s` but got `%s`", e.name, e.msg, msg)
         }
     }
+}
+
+func Test_prompt(t *testing.T) {
+    // save a copy of os.Stdout
+    oldOut := os.Stdout
+
+    // create a read and write pipe
+    r, w, _ := os.Pipe()
+
+    // set os.Stdout to our write pipe
+    os.Stdout = w
+    prompt()
+
+    // close our writer
+    _ = w.Close()
+
+    // reset os.Stdout to what it was before
+    os.Stdout = oldOut
+
+    // read the output of our prompt() func from our read pipe
+    out, _ := io.ReadAll(r)
+
+    // perform our test
+    if string(out) != "-> " {
+        t.Errorf("incorrect prompt: expected `-> ` but got %s", string(out))
+    }
+}
+
+func Test_intro(t *testing.T) {
+    // save a copy of os.Stdout
+    oldOut := os.Stdout
+
+    // create a read and write pipe
+    r, w, _ := os.Pipe()
+
+    // set os.Stdout to our write pipe
+    os.Stdout = w
+    intro()
+
+    // close our writer
+    _ = w.Close()
+
+    // reset os.Stdout to what it was before
+    os.Stdout = oldOut
+
+    // read the output of our prompt() func from our read pipe
+    out, _ := io.ReadAll(r)
+
+    // perform our test
+    if !strings.Contains(string(out), "Enter a whole number") {
+        t.Errorf("intro text is not correct;got %s", string(out))
+    }
+}
+
+func Test_checkNumbers(t *testing.T) {
+    tests := []struct {
+        name string
+        input string
+        expected string
+    } {
+        {name: "empty", input: "", expected: "Please enter a whole number!"},
+        {name: "zero", input: "0", expected: "0 is not prime, by definition!"},
+        {name: "one", input: "1", expected: "1 is not prime, by definition!"},
+        {name: "two", input: "2", expected: "2 is a prime!"},
+        {name: "negative", input: "-1", expected: "negs are not prime by definition!"},
+        {name: "types", input: "three", expected: "Please enter a whole number!"},
+        {name: "decimal", input: "1.1", expected: "Please enter a whole number!"},
+        {name: "quit", input: "q", expected: ""},
+        {name: "Q", input: "Q", expected: ""},
+        {name: "slavic", input: "ёпта", expected: "Please enter a whole number!"},
+    }
+
+    for _, e := range tests {
+        input := strings.NewReader(e.input)
+        reader := bufio.NewScanner(input)
+        res, _ := checkNumbers(reader)
+
+        if !strings.EqualFold(res, e.expected) {
+            t.Errorf("%s: expected %s, but got %s", e.name, e.expected, res)
+        }
+    }
+}
+
+func Test_readUserInput(t *testing.T) {
+       // to test this func, we need a channel, and an instance of an io.NewReader
+       doneChan := make(chan bool)
+
+       // create a ref to a bytes.Buffer
+       var stdin bytes.Buffer
+
+       stdin.Write([]byte("1\nq\n"))
+
+       go readUserInput(&stdin, doneChan)
+       <-doneChan
+       close(doneChan)
 }
