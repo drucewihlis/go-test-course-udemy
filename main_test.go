1b0ff7f737da52cbd11409312379a80a9441dd9b
anvi <avikhlyantsev@fix.ru>
2024-08-27 12:58:15 +0300

    3.14 done. with coverage

--- main_test.go
+++ main_test.go
@@ -1,24 +1,38 @@
 // go test .
+// go test -cover .
+// go test -coverprofile=coverage.out .
+// go tool cover -html=coverage.out  // should open browser, does not work for me
+// go tool cover -html=coverage.out -o coverage.html // generates html
 package main
 
 import "testing"
 
 func Test_isPrime(t *testing.T) {
-    result, msg := isPrime(0)
-    if result {
-        t.Errorf("with %d as test parameter, got true, but expected false", 0)
+    primeTests := []struct {
+        name string
+        testNum int
+        expected bool
+        msg string
+    }{
+        {"prime", 7, true, "7 is a prime!"},
+        {"not prime", 8, false, "8 is not prime, cause it is divisible by 2!"},
+        {"zero", 0, false, "0 is not prime, by definition!"},
+        {"one", 1, false, "1 is not prime, by definition!"},
+        {"negative", -1, false, "negs are not prime by definition!"},
     }
 
-    if msg != "0 is not prime, by definition!" {
-        t.Error("Wrong message returned:", msg)
-    }
+    for _, e := range primeTests {
+        result, msg := isPrime(e.testNum)
+        if e.expected && !result {
+            t.Errorf("%s: expected true but got false", e.name)
+        }
 
-    result, msg = isPrime(7)
-    if !result {
-        t.Errorf("with %d as test parameter, got false, but expected true", 7)
-    }
+        if !e.expected && result {
+            t.Errorf("%s: expected false but got true", e.name)
+        }
 
-    if msg != "7 is a prime!" {
-        t.Error("Wrong message returned:", msg)
+        if e.msg != msg {
+            t.Errorf("%s: expected `%s` but got `%s`", e.name, e.msg, msg)
+        }
     }
 }
