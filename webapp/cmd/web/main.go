5b9fb607cbfa919c90e948c80fa26fdfc94f041c
anvi <avikhlyantsev@fix.ru>
2024-08-28 10:57:52 +0300

    fix

--- webapp/cmd/web/main.go
+++ webapp/cmd/web/main.go
@@ -1,16 +1,27 @@
+// go run ./cmd/web
 package main
+
+import (
+	"log"
+	"net/http"
+)
 
 type application struct{}
 
 func main() {
-    // set up an app config
-    app := application{}
+	// set up an app config
+	app := application{}
 
-    // get app routes
-    mux := app.routes()
+	// get app routes
+	mux := app.routes()
 
-    // print out a message
+	// print out a message
+	log.Println("Starting server on port 8080...")
 
-    // start the server
+	// start the server
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 }
