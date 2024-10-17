8108d647918ec798a5cfa32d8485d8ebe147f066
anvi <avikhlyantsev@fix.ru>
2024-10-17 10:59:16 +0300

    11 .95

---
+++ webapp/cmd/api/api-handlers.go
@@ -0,0 +1,39 @@
+package main
+
+import "net/http"
+
+func (app *application) authenticate(w http.ResponseWriter, r *http.Request) {
+	// read a json payload
+
+	// look up hte user by eail address
+
+	// check pass
+
+	// generate tokens
+
+	// send token to user
+}
+
+func (app *application) refresh(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) allUsers(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) getUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) updateUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) deleteUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) insertUser(w http.ResponseWriter, r *http.Request) {
+
+}
